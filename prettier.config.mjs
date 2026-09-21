c4c93e49849e80f8c38857c05650a86f4845081c
Imad Ennouaimi <45298051+ennouaimi@users.noreply.github.com>
2026-09-21 20:23:21 +0200

    chore: configure prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+};
+
+export default config;
